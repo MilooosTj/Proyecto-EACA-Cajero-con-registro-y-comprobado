3603c76ed6892ac8821e97c85c753619b084a8b2
suare <suare@MILOS>
2025-05-18 12:37:18 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace Proyecto_EACA_Cajero_con_registro_y_comprobado
+{
+    internal class Program
+    {
+        //Proyecto EACA Cajero con registro y comprobado                ime112768
+        
+        Public enum Menu
+        {
+            Consultarcuenta = 1,
+            depositar,
+            retirar,
+            revisarhistorialIngresos,
+            revisarhistorialRetiros,
+            salir
+        }
+        static Dictionary<DateTime, Double> revisarhistorialIngresos = new Dictionary<DateTime, Double>();
+        static Dictionary<DateTime, Double> revisarhistorialRetiros = new Dictionary<DateTime, Double>();
+        static Double saldo = 0.0;
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
