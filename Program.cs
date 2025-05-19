4242e01d6a5e1b04ccb5edb581318df31aab56ac
Milooo <112768@alumnouninter.mx>
2025-05-18 18:03:07 -0600

    Modificaciones domingo

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Proyecto_EACA_Cajero_con_registro_y_comprobado
     {
         //Proyecto EACA Cajero con registro y comprobado                ime112768
         
-        Public enum Menu
+        public enum Menu
         {
             Consultarcuenta = 1,
             depositar,
@@ -23,9 +23,30 @@ namespace Proyecto_EACA_Cajero_con_registro_y_comprobado
         static Dictionary<DateTime, Double> revisarhistorialIngresos = new Dictionary<DateTime, Double>();
         static Dictionary<DateTime, Double> revisarhistorialRetiros = new Dictionary<DateTime, Double>();
         static Double saldo = 0.0;
+
+
         static void Main(string[] args)
         {
 
+            while (true)
+            {
+
+            }
         }
+        static Menu Men()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("1) Consultar saldo actual");
+            Console.WriteLine("2) Depositar dinero");
+            Console.WriteLine("3) Retirar dinero");
+            Console.WriteLine("4) Revisar historial de depósitos");
+            Console.WriteLine("5) Revisar historial de retiros");
+            Console.WriteLine("6) Salir");
+            Console.WriteLine("------------------------------");
+            Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
+            return opc;
+        }
+
+
     }
 }
